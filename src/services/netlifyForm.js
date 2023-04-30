7086bc043e656e88be009e2de9960d4509e8ef17
yanaleso <ktcmjzyfa@gmail.com>
2023-04-30 15:07:55 +0300

    Add netlify form

---
+++ src/services/netlifyForm.js
@@ -0,0 +1,15 @@
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
+export const handleSubmit = credentials => {
+  fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: encode({ 'form-name': 'contact', ...credentials }),
+  })
+    .then(() => alert('Success!'))
+    .catch(error => alert(error))
+}
